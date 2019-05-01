8d8dfbedddb4e7d513a7a721ebfea2630e47d272
Kathleen Dollard <kathleen.a.dollard@gmail.com>
2019-04-30 19:51:32 -0700

    First iteration of Demo

---
+++ code/MyApp/MyApp/StringFiller.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace MyApp
+{
+    public class StringFiller
+    {
+        public string GetString(string textIn, int length)
+        {
+            var output = "";
+            var text = textIn.ToList();
+            for (var j = 0; j < text.Count(); j++)
+            {
+                for (var i = 0; i < (length >= text.Count()
+                                          ? length / text.Count()
+                                          : 1); i++)
+                {
+                    output = AppendIfNotTooLong(length, output, text, j);
+                }
+            }
+            return output;
+        }
+
+        private static string AppendIfNotTooLong(int length, string output, System.Collections.Generic.List<char> text, int j)
+        {
+            if (output.Count() <= length)
+            {
+                output += text[j];
+            }
+
+            return output;
+        }
+    }
+}
