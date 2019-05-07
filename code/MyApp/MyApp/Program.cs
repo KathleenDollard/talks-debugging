0beca9bbccfe4dbcbe48482f5383484a4e1de8f7
Kathleen Dollard <kathleen.a.dollard@gmail.com>
2019-05-06 17:45:41 -0700

    Clean program.cs and adjusted values

--- code/MyApp/MyApp/Program.cs
+++ code/MyApp/MyApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Threading;
 
 namespace MyApp
 {
@@ -7,22 +8,16 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
-            if (!Int32.TryParse(args[0], out int length))
-            {
-                Console.WriteLine("Please enter the number of iterations");
-                Console.Read();
-                return;
-            };
             string text = args[1];
             var filler = new StringFiller();
-            string output = filler.GetString(Gettysburg_long, 10_000);
+            string output = filler.GetString(Gettysburg_long, 30_000);
             Console.WriteLine(output.ToString());
         }
 
         private static readonly string _gettysburg = "Fourscore and seven years ago our fathers brought forth, on this continent, a new nation, conceived in liberty, and dedicated to the proposition that all men are created equal.Now we are engaged in a great civil war, testing whether that nation, or any nation so conceived, and so dedicated, can long endure.We are met on a great battle-field of that war. We have come to dedicate a portion of that field, as a final resting-place for those who here gave their lives, that that nation might live.It is altogether fitting and proper that we should do this. But, in a larger sense, we cannot dedicate, we cannot consecrate—we cannot hallow—this ground.The brave men, living and dead, who struggled here, have consecrated it far above our poor power to add or detract. The world will little note, nor long remember what we say here, but it can never forget what they did here.It is for us the living, rather, to be dedicated here to the unfinished work which they who fought here have thus far so nobly advanced.It is rather for us to be here dedicated to the great task remaining before us—that from these honored dead we take increased devotion to that cause for which they here gave the last full measure of devotion—that we here highly resolve that these dead shall not have died in vain—that this nation, under God, shall have a new birth of freedom, and that government of the people, by the people, for the people, shall not perish from the earth.";
 
         private static string Gettysburg_long =>
-           String.Join('|', Enumerable.Range(0, 10)
+           String.Join('|', Enumerable.Range(0, 100)
               .Select(x => _gettysburg));
     }
 }
